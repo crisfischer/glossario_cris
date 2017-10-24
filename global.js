95cd9645fa2fb96a082d8a59ffb520c490dcc7e6
crifischer <cristianealinefischer@gmail.com>
2017-10-24 18:06:00 -0200

    Documentação materialize

--- global.js
+++ global.js
@@ -1,41 +1,41 @@
 $(document).ready(function(){
     var videos = [
-        {
-        value: "Antiderrapante",
-        label: "antiderrapante",
-        desc: "Antiderrapante para não cair",
-        video_: "antiderrapante_sinal.mp4"
-        },
-        {
-        value: "Azulejo",
-        label: "Azulejo",
-        desc: "Azulejo para revestir a casa",
-        video_: "azulejo_sinal.mp4",
-        },
-        {
-        value: "Banheira",
-        label: "banheira",
-        desc: "Banheira para tomar banho",
-        video_: "banheira_sinal.mp4",
-        },
-        {
-        value: "Boneca",
-        label: "boneca",
-        desc: "Boneca para brincar",
-        video_: "boneca_sinal.mp4",
-        },
-        {
-        value: "Coluna",
-        label: "coluna",
-        desc: "Coluna do ser humano",
-        video_: "coluna_sinal.mp4",
-        },
-        {
-        value: "Janela",
-        label: "janela",
-        desc: "Janela de casa",
-        video_: "janela_sinal.mp4",
-        }
+                    {
+                    value: "Antiderrapante",
+                    label: "antiderrapante",
+                    desc: "Antiderrapante para não cair",
+                    video_: "antiderrapante_sinal.mp4"
+                    },
+                    {
+                    value: "Azulejo",
+                    label: "Azulejo",
+                    desc: "Azulejo para revestir a casa",
+                    video_: "azulejo_sinal.mp4",
+                    },
+                    {
+                    value: "Banheira",
+                    label: "banheira",
+                    desc: "Banheira para tomar banho",
+                    video_: "banheira_sinal.mp4",
+                    },
+                    {
+                    value: "Boneca",
+                    label: "boneca",
+                    desc: "Boneca para brincar",
+                    video_: "boneca_sinal.mp4",
+                    },
+                    {
+                    value: "Coluna",
+                    label: "coluna",
+                    desc: "Coluna do ser humano",
+                    video_: "coluna_sinal.mp4",
+                    },
+                    {
+                    value: "Janela",
+                    label: "janela",
+                    desc: "Janela de casa",
+                    video_: "janela_sinal.mp4",
+                    }
     ];
 
 
