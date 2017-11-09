8c3f6f29c01cb857f2d5bd15c4dcd37d49d9cd2f
crifischer <cristianealinefischer@gmail.com>
2017-11-09 18:23:53 -0200

    lista de videos

--- global.js
+++ global.js
@@ -5,35 +5,40 @@
         label: "antiderrapante",
         desc: "Antiderrapante para não cair",
         videoSinal: "antiderrapante_sinal.mp4",
-        videoConceito: "azulejo_sinal.mp4"
+        videoConceito: "azulejo_sinal.mp4",
+        area:"Letras Libras"
         },
         {
         value: "Azulejo",
         label: "Azulejo",
         desc: "Azulejo para revestir a casa",
         videoSinal: "azulejo_sinal.mp4",
-        videoConceito: "antiderrapante_sinal.mp4"
+        videoConceito: "antiderrapante_sinal.mp4",
+        area:"Letras Libras"
         },
         {
         value: "Banheira",
         label: "banheira",
         desc: "Banheira para tomar banho",
         videoSinal: "janela_sinal.mp4",
-        videoConceito: "antiderrapante_sinal.mp4"
+        videoConceito: "antiderrapante_sinal.mp4",
+        area:"Psicologia"
         },
         {
         value: "Boneca",
         label: "boneca",
         desc: "Boneca para brincar",
         videoSinal: "coluna_sinal.mp4",
-        videoConceito: "banheira_sinal.mp4"
+        videoConceito: "banheira_sinal.mp4",
+        area: "Psicologia"
         },
         {
         value: "Coluna",
         label: "coluna",
         desc: "Coluna do ser humano",
         videoSinal: "coluna_sinal.mp4",
-        videoConceito: "janela_sinal.mp4"
+        videoConceito: "janela_sinal.mp4",
+        area: "Arquitetura"
         },
         {
         value: "Janela",
@@ -44,10 +49,29 @@
         area: "Arquitetura"
         }
     ];
+    var areas = ['Letras Libras','Arquitetura','Pscicologia'];
+        for(var j=0;j<areas.length;j++){
+            if(areas[j]['area'] == 'Letras Libras'){
+            $("#classificaLibras").append(" id='menu_"+areas[j]+"' <a href='#'> "+areas[j]+"</a>");
+            };   
+            if(areas[j]['area'] == 'Arquitetura'){
+            $(".classifica").append(" id='menu_"+areas[j]+"' <a href='#'> "+areas[j]+"</a>");
+            };
+            if(areas[j]['area'] == 'Psicologia'){
+            $(".classifica").append(" id='menu_"+areas[j]+"' <a href='#'> "+areas[j]+"</a>");
+            };
+    };
     for(var i=0;i<videos.length;i++){
-        $("#menu_video").append("<li><a id='"+videos[i]['value']+"' href='#'> "+videos[i]['value']+"</a></li>");
+        if(videos[i]['area'] == 'Letras Libras'){
+        $("#menu_videoLetras").append("<li><a id='"+videos[i]['value']+"' href='#'> "+videos[i]['value']+"</a></li>");
+        };   
+        if(videos[i]['area'] == 'Arquitetura'){
+        $("#menu_videoArquitetura").append("<li><a id='"+videos[i]['value']+"' href='#'> "+videos[i]['value']+"</a></li>");
+        };   
+        if(videos[i]['area'] == 'Psicologia'){
+        $("#menu_videoPsicologia").append("<li><a id='"+videos[i]['value']+"' href='#'> "+videos[i]['value']+"</a></li>");
+        };   
         $("#"+videos[i]['value']).click({id:i}, trocaVideo);
-        
     };
     function trocaVideo(event) {
         $("#video_play_sinal").attr("src","videos/" + videos[event.data.id]["videoSinal"]);
@@ -56,12 +80,7 @@
         $("#video_play_conceito")[0].play();
         $("#nomes").html("<li><a id='"+videos[event.data.id]['value']+"' href='#'> "+videos[event.data.id]['value']+"</a></li>");
     };
-    var tipos = ['Letras Libras','Arquitetura','Pscicologia'];
-
-    for(var j=0;j<tipos.length;j++){
-        $("#").append(" id='menu_"+tipos[j]+"' href='#'> "+tipos[j]+"");
-    };
-
+    
     $(function(){
         $(".button-collapse").sideNav();
      });
