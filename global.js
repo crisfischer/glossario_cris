da5c9d6d577b32f8de3354cd57b30c6c6fe17b20
crifischer <cristianealinefischer@gmail.com>
2017-12-05 15:06:21 -0200

    Interface separada_bug

--- global.js
+++ global.js
@@ -77,4 +77,15 @@
     });
     $(document).ready(function(){
     $('.collapsible').collapsible();
-  });  
+  }); 
+   $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  }); 
+    $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+    
+
+    
+   
+
